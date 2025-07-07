43cb1048652d17aec40f4c6babe767ffcd9eccde
Parth9542 <110736114+Parth9542@users.noreply.github.com>
2025-07-08 00:58:22 +0530

    Update styles.js

--- parth-box/styles.css/styles.js
+++ parth-box/styles.css/styles.js
@@ -1,8 +1,1 @@
-document.querySelectorAll(".box").forEach((box) => {
-  box.addEventListener("click", () => {
-    document.querySelectorAll(".box").forEach((b) => {
-      if (b !== box) b.classList.remove("expanded");
-    });
-    box.classList.toggle("expanded");
-  });
-});
+

d3edaf1b88dac55247aee2185cafae9c4d1beb69
Parth9542 <110736114+Parth9542@users.noreply.github.com>
2025-07-08 00:57:47 +0530

    Update script.js

--- parth-box/script.js/script.js
+++ parth-box/script.js/script.js
@@ -1,11 +1,1 @@
-document.querySelectorAll(".box").forEach((box) => {
-  box.addEventListener("click", () => {
-    // Collapse all other boxes
-    document.querySelectorAll(".box").forEach((b) => {
-      if (b !== box) b.classList.remove("expanded");
-    });
 
-    // Toggle clicked box
-    box.classList.toggle("expanded");
-  });
-});
